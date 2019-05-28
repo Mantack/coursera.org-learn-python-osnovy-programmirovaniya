e11666c60c8e09f01aac45afa82a4101b9d1e855
Mukhtar <Wwpeit@gmail.com>
2019-05-28 19:17:59 +0600

    !

---
+++ hello.py
@@ -0,0 +1,63 @@
+# name = input()
+# pin = ' "' +   '_~_'\n+'  (o o)\n',' /  V  \\\n','/(  _  )\\\n','  ^^ ^^")'
+# print(pin)
+# n = int(input())
+# m = int(input())
+# print(m%n)
+# name = input()
+# print("Hello, ", name, "!", sep="")
+# n = int(input())
+# print(n // 60 % 24)
+# print(n % 60)
+# a = int(input())
+# b = int(input())
+# n = int(input())
+# c = (a * 100 + b) * n
+# print(c // 100, c % 100)
+# nnn = int(input())
+# n = (nnn // 10) % 10
+# print(n)
+
+# v = int(input())
+# a1 = '   _~_    '
+# a2 = '  (o o)   '
+# a3 = ' /  V  \  '
+# a4 = '/(  _  )\ '
+# a5 = '  ^^ ^^   '
+# print(a1*v)
+# print(a2*v)
+# print(a3*v)
+# print(a4*v)
+# print(a5*v)
+
+# nnn = int(input())
+# print((nnn // 10) % 10)
+
+# v = int(input())
+# print("The next number for the number ", v, " is ", v + 1, ".", sep="")
+# print("The previous number for the number ", v, " is ", v - 1, ".", sep="")
+
+# v = int(input())
+# if v == 0:
+# 	print(1)
+# else:
+# 	print(0)
+
+# v = int(input())
+# if v % 2 == 0:
+#     print(v + 2)
+# else:
+#     print(v + 1)
+
+
+# n = int(input())
+# n = str(n) * 100
+# n = int(n) ** 2
+# print(n)
+
+v = int(input())
+print(int((str(v) * 100)) ** 2)
+
+
+
+
